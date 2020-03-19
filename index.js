704e95a36a011610be4c23a36183470b62491974
Anders <akerberganders@gmail.com>
2020-03-19 14:35:07 +0100

    Fixed errors

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
 const core = require('@actions/core');
 try {
   const openapidiff = core.getInput('openapidiff');
-  const asJson = JSON.parse(openapidiff);
-  let breakingChanges = [];
-  if (asJson.breakingDifferences) {
-    asJson.breakingDifferences.forEach(breakingChange => {
-      if (breakingChange.sourceSpecEntityDetails) {
-        breakingChange.sourceSpecEntityDetails.forEach(
-          sourceSpecEntityDetail => {
-            if (
-              sourceSpecEntityDetail.location &&
-              sourceSpecEntityDetail.value
-            ) {
-              breakingChanges.push({
-                location: sourceSpecEntityDetail.location,
-                value: sourceSpecEntityDetail.value
-              });
-            }
-          }
-        );
-      }
-    });
-  }
-  const returnValue = '| Path | Value |\n|-------------|-------------|\n|';
+  if (openapidiff && openapidiff.length > 0) {
+    openapidiff = openapidiff.replace(
+      'Breaking changes found between the two specifications:',
+      ''
+    );
 
-  if (breakingChanges.length == 0) {
-    returnValue += ` N/A | No breaking changes reported `;
+    const asJson = JSON.parse(openapidiff);
+    let breakingChanges = [];
+    if (asJson.breakingDifferences) {
+      asJson.breakingDifferences.forEach(breakingChange => {
+        if (breakingChange.sourceSpecEntityDetails) {
+          breakingChange.sourceSpecEntityDetails.forEach(
+            sourceSpecEntityDetail => {
+              if (
+                sourceSpecEntityDetail.location &&
+                sourceSpecEntityDetail.value
+              ) {
+                breakingChanges.push({
+                  location: sourceSpecEntityDetail.location,
+                  value: sourceSpecEntityDetail.value
+                });
+              }
+            }
+          );
+        }
+      });
+    }
+    const returnValue = '| Path | Value |\n|-------------|-------------|\n|';
+
+    if (breakingChanges.length == 0) {
+      returnValue += ` N/A | No breaking changes reported `;
+    } else {
+      breakingChanges.forEach(change => {
+        returnValue += `${change.location} | ${JSON.stringify(
+          change.location,
+          null,
+          4
+        )} |\n`;
+      });
+    }
+    core.setOutput('openapidiffmarkdown', returnValue);
   } else {
-    breakingChanges.forEach(change => {
-      returnValue += `${change.location} | ${JSON.stringify(
-        change.location,
-        null,
-        4
-      )} |\n`;
-    });
+    throw 'Invalid input please send JSON!';
   }
-  core.setOutput('openapidiffmarkdown', returnValue);
 } catch (error) {
   core.setFailed(error.message);
 }
